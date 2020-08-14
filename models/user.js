9a2b4ed02fdec9e7cf0e10efcb607d8f2bdde245
baluzealster <adusumalli.balakrishna@gmail.com>
2020-08-14 21:06:34 +0530

    WIP

--- models/user.js
+++ models/user.js
@@ -1,1 +1,23 @@
 const sql = require("./db");
+const { query } = require("./db");
+const User = function (user) {
+  (this.name = user.name),
+    (this.email = user.email),
+    (this.phone = user.phone),
+    (this.profilePic = user.profilePic);
+};
+
+User.create = (newUser, result) => {
+  query = {
+    name: newUser.name,
+    email: newUser.email,
+  };
+  sql.query("INSERT INTO users SET ?", query, (err, res) => {
+    if (err) {
+      console.log("error", err);
+      result(err);
+      return;
+    }
+    result(null, res[0]);
+  });
+};
